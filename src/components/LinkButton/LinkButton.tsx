9abceca782f89145913f17f25abda6ab13f93f6b
Oleksii Shevchenko(Mac) <rembo.51423@gmail.com>
2024-05-24 20:15:12 +0300

    feat(components): add button className builder

--- src/components/LinkButton/LinkButton.tsx
+++ src/components/LinkButton/LinkButton.tsx
@@ -1,5 +1,4 @@
-import { lexend } from '@/constants/fonts';
-import clsx from 'clsx';
+import { getButtonClassName } from '@/utils/getBtnClassName';
 import Image from 'next/image';
 
 interface ButtonProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
@@ -15,17 +14,7 @@ export default function LinkButton({
   ...rest
 }: ButtonProps) {
   return (
-    <a
-      {...rest}
-      className={clsx(
-        `${lexend.className} flex gap-2 items-center border-[1px]  text-[17px]  py-[10px] px-[30px] rounded-[45px]  easy-in duration-300 hover:cursor-pointer`,
-        {
-          'bg-red-500 border-red-500 text-white hover:bg-white hover:text-red-500':
-            !invert,
-          'bg-white border-white text-red-500 hover:border-red-500': invert,
-        },
-      )}
-    >
+    <a {...rest} className={getButtonClassName(invert)}>
       {iconSrc && (
         <Image src={iconSrc} alt="Sign-in button icon" width={19} height={19} />
       )}
