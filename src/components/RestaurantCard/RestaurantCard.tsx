c11765f8ebb2dbd1ac5e99c59f1bb9226c51dda2
Oleksii Shevchenko(Mac) <rembo.51423@gmail.com>
2024-05-28 18:36:15 +0300

    style(comps/RestaurantCad): redesign restaurant card

--- src/components/RestaurantCard/RestaurantCard.tsx
+++ src/components/RestaurantCard/RestaurantCard.tsx
@@ -1,36 +1,47 @@
 import { IRestaurant } from '@/types/restaurant.type';
 import Image from 'next/image';
+import React from 'react';
 
 type Props = {
   restaurant: IRestaurant;
 };
 
 export default function RestaurantCard({ restaurant }: Props) {
-  const { imageUrl, title, rating, cuisine, description, _res_id } = restaurant;
+  const { title, rating, cuisine, description, _id } = restaurant;
 
   return (
-    <div className="max-h-[420px] max-w-[370px]  overflow-hidden rounded-xl bg-white text-black shadow-lg duration-[400ms] hover:scale-105">
-      <a href={`/restaurants/${_res_id}`}>
+    <div className="h-[345px] w-[250px]  overflow-hidden rounded-xl bg-white text-black shadow-lg duration-[400ms] hover:scale-105">
+      <a href={`/restaurants/${_id}`} className="flex h-full flex-col">
         <Image
-          className="max-h-[247px] w-full object-cover"
-          src={imageUrl}
-          width={150}
-          height={150}
+          className="max-h-[170px] w-full max-w-[250px] object-cover"
+          src={'/img/example-resPhoto.jpg'}
+          width={250}
+          height={170}
           alt={`${title} restaurant photo`}
         />
-        <div className="px-6 py-4">
-          <h4 className={` mb-2 text-xl font-semibold`}>{title}</h4>
-          <p className="mb-1 text-base text-gray-700">
-            <span className="font-semibold">Cuisine:</span> {cuisine}
-          </p>
-          <p className="mb-1 text-base text-gray-700">
-            <span className="font-semibold">Rating:</span> {rating} / 5
-          </p>
-          <p className=" max-h-12 truncate text-[14px] text-base text-gray-700">
+        <div className=" h-full w-full flex-1  px-6 py-4">
+          <h4 className={` mb-2 h-[48px] text-base font-semibold`}>{title}</h4>
+          <CardItem itemName="Cuisine">{cuisine.join(', ')}</CardItem>
+          <CardItem itemName="Rating">{rating} / 5</CardItem>
+          <p className=" max-h-12 truncate text-sm italic text-gray-700">
             {description}
           </p>
         </div>
       </a>
     </div>
+  );
+}
+
+function CardItem({
+  children,
+  itemName,
+}: {
+  children: React.ReactNode;
+  itemName: string;
+}) {
+  return (
+    <p className="mb-1 overflow-hidden text-ellipsis text-sm text-gray-700">
+      <span className="mr-1 font-semibold">{itemName}:</span> {children}
+    </p>
   );
 }
