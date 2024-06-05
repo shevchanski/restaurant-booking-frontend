8b992b2825f46f2b299ee6950d4d70fce6ace1f8
Oleksii Shevchenko(Mac) <rembo.51423@gmail.com>
2024-06-05 22:17:26 +0300

    feat(app/register_restaurant): create page to register restaurant

---
+++ src/app/register_restaurant/[[...register_restaurant]]/page.tsx
@@ -0,0 +1,20 @@
+import Footer from '@/components/Footer/Footer';
+import Header from '@/components/Header/Header';
+import MainSection from '@/components/MainSection/MainSection';
+import RestaurantForm from '@/components/RestaurantForm/RestaurantForm';
+
+export default function RestaurantRegistrationPage() {
+  return (
+    <>
+      <Header />
+
+      <div className="absolute left-0 top-0 z-[-99] h-full min-h-screen w-[100vw] bg-[url(/img/restRegistrationPage-bg.jpg)] bg-cover"></div>
+
+      <MainSection>
+        <RestaurantForm />
+      </MainSection>
+
+      <Footer />
+    </>
+  );
+}
