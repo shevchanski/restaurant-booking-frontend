6ea390c2ff0a37e618de232ad0405baffb28a94c
Oleksii Shevchenko(Mac) <rembo.51423@gmail.com>
2024-06-05 22:02:20 +0300

    feat(comps/MainSection): add innerBoxStyle & mainBoxStyle

--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -1,13 +1,22 @@
+import clsx from 'clsx';
 import Wrapper from '../Wrapper/Wrapper';
 
-interface Props extends React.HTMLAttributes<HTMLElement> {
+interface Props {
   children: React.ReactNode;
+  mainBoxStyle?: string;
+  innerBoxStyle?: string;
 }
 
-export default function MainSection({ children, ...rest }: Props) {
+export default function MainSection({
+  children,
+  mainBoxStyle,
+  innerBoxStyle,
+}: Props) {
   return (
-    <main className=" w-full flex-1 py-20 text-black" {...rest}>
-      <Wrapper>{children}</Wrapper>
+    <main className={clsx(mainBoxStyle, ' w-full flex-1 py-20 text-black')}>
+      <Wrapper>
+        <div className={innerBoxStyle}>{children}</div>
+      </Wrapper>
     </main>
   );
 }
