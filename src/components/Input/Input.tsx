92e36ed5234e05a57bca2a3132add8112b4ad2ce
Oleksii Shevchenko(Mac) <rembo.51423@gmail.com>
2024-06-05 22:00:06 +0300

    feat(components/Input): add more props as required|errorMessage

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -5,6 +5,7 @@ import React, { forwardRef } from 'react';
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   labelText: React.ReactNode;
   inputIcon?: React.ReactNode;
+  errorMessage?: string;
 }
 
 /**
@@ -13,33 +14,43 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
  * @returns {React.ReactNode}
  */
 const Input = forwardRef<HTMLInputElement, Props>(function Input(
-  { labelText, className, inputIcon, ...rest }: Props,
+  { labelText, className, inputIcon, required, errorMessage, ...rest }: Props,
   ref,
 ) {
   const _id = uniqueHtmlId('input-');
 
   return (
-    <div>
+    <div className={className}>
       <label
         htmlFor={_id}
-        className="mb-3 block text-xs font-medium text-gray-900"
+        className="mb-2 block text-sm font-medium text-gray-900"
       >
         {labelText}
+        {required ? (
+          <span className=" ml-1 text-red-600">*</span>
+        ) : (
+          <span className="ml-2 text-[8px] leading-none">(optional)</span>
+        )}
       </label>
       <div className="relative">
         <input
           className={clsx(
-            className,
-            'aria-disabled: peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500',
+            'aria-disabled: peer block w-full rounded-md border border-gray-200 py-[9px] text-sm outline-2 placeholder:text-gray-500',
+            { 'pl-10': inputIcon },
           )}
           id={_id}
           ref={ref}
+          required={required}
           {...rest}
         />
-        <div className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900">
-          {inputIcon}
-        </div>
+
+        {inputIcon && (
+          <div className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900">
+            {inputIcon}
+          </div>
+        )}
       </div>
+      {errorMessage && <p className="">{errorMessage}</p>}
     </div>
   );
 });
