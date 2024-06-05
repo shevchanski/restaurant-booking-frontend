dd66082a55c30489a26a4e145e4484f460a5fa4a
Oleksii Shevchenko(Mac) <rembo.51423@gmail.com>
2024-06-06 00:31:58 +0300

    refactor(utils): add default values for params for generateSearchParams

--- src/utils/generateSearchParams.ts
+++ src/utils/generateSearchParams.ts
@@ -1,13 +1,19 @@
+import {
+  DEFAULT_PAGE,
+  DEFAULT_PER_PAGE,
+  DEFAULT_SORT_BY,
+  DEFAULT_SORT_OPTION,
+} from '@/constants/pagination';
 import { SearchParams } from '@/types/search.type';
 
 const generateSearchParams = ({
-  searchTerm,
-  page,
-  perPage,
-  sortBy,
-  sortOption,
+  searchTerm = undefined,
+  page = DEFAULT_PAGE,
+  perPage = DEFAULT_PER_PAGE,
+  sortBy = DEFAULT_SORT_BY,
+  sortOption = DEFAULT_SORT_OPTION,
 }: SearchParams) => {
   return `?${!!searchTerm ? `searchTerm=${searchTerm}` : ''}&page=${page}&perPage=${perPage}&sortBy=${sortBy}&sortOption=${sortOption}`;
 };
 
-export default generateSearchParams;
+export { generateSearchParams };
