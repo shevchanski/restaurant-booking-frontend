d281d51f79bcb8fb4c989284bda80fd3073addb0
Oleksii Shevchenko(Mac) <rembo.51423@gmail.com>
2024-06-07 00:12:28 +0300

    refact(app/restaurant): change main+Wrapper -> MainSection

--- src/app/restaurants/[restaurantId]/page.tsx
+++ src/app/restaurants/[restaurantId]/page.tsx
@@ -2,9 +2,9 @@
 
 import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
+import MainSection from '@/components/MainSection/MainSection';
 import RatingNumber from '@/components/RatingNumber/RatingNumber';
 import RestPhotoCarousel from '@/components/RestPhotoCarousel/RestPhotoCarousel';
-import Wrapper from '@/components/Wrapper/Wrapper';
 import { atma } from '@/constants/fonts';
 import fetchRestaurantById from '@/utils/fetchRestaurantById';
 import {
@@ -33,77 +33,75 @@ export default async function RestaurantPage({
     <>
       <Header />
 
-      <main className=" w-full flex-1 py-20 text-black">
-        <Wrapper>
-          {restaurant ? (
-            <div className="grid-rows-[minmax(350px, 1fr)_1fr]  grid h-full w-full grid-cols-8 gap-3 gap-y-5">
-              <RestPhotoCarousel
-                className="col-span-6 col-start-2"
-                restId={restaurantId}
-              />
-              <div className="col-span-4 col-start-2 min-h-[350px] rounded-l-md bg-gradient-to-r from-red-500 p-[1px] pr-0">
-                <div className="h-full rounded-l-md bg-white p-5  ">
-                  <div className="flex justify-between">
-                    <h2 className={clsx(atma.className, 'ml-2 text-3xl')}>
-                      {restaurant.title}
-                    </h2>
-                    <RatingNumber
-                      rating={restaurant.rating}
-                      className="text-lg"
-                    />
-                  </div>
-                  <hr className="mb-4 mt-2 h-[1px] w-6/12 border-0 bg-gradient-to-r from-red-500 " />
-                  <p className="mb-2 ml-4">
-                    <span className=" mr-1 font-semibold ">
-                      Cuisine{restaurant.cuisine.length > 1 ? 's' : ''}:
-                    </span>
-                    {Object.values(restaurant.cuisine).join(', ')}
-                  </p>
-                  <p className="first-letter:font-bold: text-base first-letter:ml-4 ">
-                    {restaurant.description}
-                  </p>
+      <MainSection>
+        {restaurant ? (
+          <div className="grid-rows-[minmax(350px, 1fr)_1fr]  grid h-full w-full grid-cols-8 gap-3 gap-y-5">
+            <RestPhotoCarousel
+              className="col-span-6 col-start-2"
+              restId={restaurantId}
+            />
+            <div className="col-span-4 col-start-2 min-h-[350px] rounded-l-md bg-gradient-to-r from-red-500 p-[1px] pr-0">
+              <div className="h-full rounded-l-md bg-white p-5  ">
+                <div className="flex justify-between">
+                  <h2 className={clsx(atma.className, 'ml-2 text-3xl')}>
+                    {restaurant.title}
+                  </h2>
+                  <RatingNumber
+                    rating={restaurant.rating}
+                    className="text-lg"
+                  />
                 </div>
+                <hr className="mb-4 mt-2 h-[1px] w-6/12 border-0 bg-gradient-to-r from-red-500 " />
+                <p className="mb-2 ml-4">
+                  <span className=" mr-1 font-semibold ">
+                    Cuisine{restaurant.cuisine.length > 1 ? 's' : ''}:
+                  </span>
+                  {Object.values(restaurant.cuisine).join(', ')}
+                </p>
+                <p className="first-letter:font-bold: text-base first-letter:ml-4 ">
+                  {restaurant.description}
+                </p>
               </div>
+            </div>
 
-              <div className="col-span-2 rounded-r-md bg-gradient-to-l from-red-500 p-[1px]  pl-0">
-                <div className="h-full rounded-r-md bg-white p-5 pl-1">
-                  <div className=" grid grid-cols-[24px_minmax(0,1fr)] gap-x-1 gap-y-5">
-                    <MapPinIcon className="inline h-6 w-6 text-zinc-500" />
-                    <RightSideListItem hoverUnderline={false}>
-                      {Object.values(restaurant.address).join(', ')}
-                    </RightSideListItem>
-                    {restaurant.website && (
-                      <>
-                        <LinkIcon className=" inline h-5 w-5  text-zinc-500 " />
-                        <RightSideListItem href={restaurant.website}>
-                          {restaurant.website}
-                        </RightSideListItem>
-                      </>
-                    )}
-                    <PhoneIcon className="inline h-5 w-5 text-zinc-500" />
-                    <RightSideListItem href={`tel:${restaurant.phoneNumber}`}>
-                      {restaurant.phoneNumber}
-                    </RightSideListItem>
-                    <PuzzlePieceIcon className="inline h-5 w-5 text-zinc-500" />
-                    <RightSideListItem hoverUnderline={false}>
-                      Joined on{' '}
-                      {restaurant.createdAt
-                        .split('T')
-                        .shift()
-                        ?.toString()
-                        .split('-')
-                        .reverse()
-                        .join('.')}
-                    </RightSideListItem>
-                  </div>
+            <div className="col-span-2 rounded-r-md bg-gradient-to-l from-red-500 p-[1px]  pl-0">
+              <div className="h-full rounded-r-md bg-white p-5 pl-1">
+                <div className=" grid grid-cols-[24px_minmax(0,1fr)] gap-x-1 gap-y-5">
+                  <MapPinIcon className="inline h-6 w-6 text-zinc-500" />
+                  <RightSideListItem hoverUnderline={false}>
+                    {Object.values(restaurant.address).join(', ')}
+                  </RightSideListItem>
+                  {restaurant.website && (
+                    <>
+                      <LinkIcon className=" inline h-5 w-5  text-zinc-500 " />
+                      <RightSideListItem href={restaurant.website}>
+                        {restaurant.website}
+                      </RightSideListItem>
+                    </>
+                  )}
+                  <PhoneIcon className="inline h-5 w-5 text-zinc-500" />
+                  <RightSideListItem href={`tel:${restaurant.phoneNumber}`}>
+                    {restaurant.phoneNumber}
+                  </RightSideListItem>
+                  <PuzzlePieceIcon className="inline h-5 w-5 text-zinc-500" />
+                  <RightSideListItem hoverUnderline={false}>
+                    Joined on{' '}
+                    {restaurant.createdAt
+                      .split('T')
+                      .shift()
+                      ?.toString()
+                      .split('-')
+                      .reverse()
+                      .join('.')}
+                  </RightSideListItem>
                 </div>
               </div>
             </div>
-          ) : (
-            <p></p>
-          )}
-        </Wrapper>
-      </main>
+          </div>
+        ) : (
+          <p></p>
+        )}
+      </MainSection>
 
       <Footer />
     </>
