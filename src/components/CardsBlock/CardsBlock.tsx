7ccd00818f921055df854549b261d4a8ae0349fd
Oleksii Shevchenko(Mac) <rembo.51423@gmail.com>
2024-06-07 00:16:03 +0300

    feat(comps/CardsBlock): add prop className & add CardsGrid

--- src/components/CardsBlock/CardsBlock.tsx
+++ src/components/CardsBlock/CardsBlock.tsx
@@ -1,24 +1,25 @@
 import { IRestaurant } from '@/types/restaurant.type';
+import clsx from 'clsx';
+import CardsGrid from '../CardsGrid/CardsGrid';
 import H2 from '../H2/H2';
-import RestaurantCard from '../RestaurantCard/RestaurantCard';
-import Wrapper from '../Wrapper/Wrapper';
 
 type Props = {
   titleOfBlock: string;
   restaurants: IRestaurant[];
+  className?: string;
 };
 
-export default function CardsBlock({ titleOfBlock, restaurants }: Props) {
+export default function CardsBlock({
+  titleOfBlock,
+  restaurants,
+  className,
+}: Props) {
   return (
-    <div className="bg-white pb-[60px] pt-[60px]">
-      <Wrapper>
-        <H2>{titleOfBlock}</H2>
-        <div className="mx-auto mt-16 flex w-fit flex-wrap gap-8  px-[60px]">
-          {...restaurants.map((restaurant, i) => (
-            <RestaurantCard key={i} restaurant={restaurant} />
-          ))}
-        </div>
-      </Wrapper>
+    <div className={clsx(className, 'flex flex-col items-center bg-white')}>
+      <H2>{titleOfBlock}</H2>
+      <div className="mx-auto mt-5 flex w-fit flex-wrap gap-8 px-[60px]">
+        <CardsGrid restaurants={restaurants} />
+      </div>
     </div>
   );
 }
