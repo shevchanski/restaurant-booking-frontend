5647617d1a05a6343622bfc34af7600aa3936d95
Oleksii Shevchenko(Mac) <rembo.51423@gmail.com>
2024-06-06 20:07:45 +0300

    feat(utils/getUserFavorites): add returning type & return restaurants[]

--- src/utils/getUserFavorites.ts
+++ src/utils/getUserFavorites.ts
@@ -1,22 +1,22 @@
+'use server';
+
 import { API_URI } from '@/constants/config';
 import { IRestaurant } from '@/types/restaurant.type';
 import axios from 'axios';
 import path from 'path';
 import logFetchError from './logFetchError';
 
-async function getUserFavorites(userId: string) {
+async function getUserFavorites(userId: string): Promise<IRestaurant[]> {
   try {
     const response = await axios.get(path.join(API_URI, 'favorites', userId));
 
     if (response.data && response.data.restaurants) {
-      const restaurants: IRestaurant[] = response.data.restaurants;
-      return restaurants.map((rest) => rest._id);
+      return response.data.restaurants ?? [];
     }
-
-    return [];
   } catch (error) {
     logFetchError(error);
   }
+  return [];
 }
 
 export default getUserFavorites;
