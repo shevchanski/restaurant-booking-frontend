5c13cffee94a5262cfe496d7ca4695cd5ba8e490
Oleksii Shevchenko(Mac) <rembo.51423@gmail.com>
2024-06-06 19:56:51 +0300

    style(comps/LoadingSpinner): add width & heigth equal to spiner's & add className prop

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,6 +1,14 @@
-export default function LoadingSpinner() {
+import clsx from 'clsx';
+
+interface Props {
+  className?: string;
+}
+
+export default function LoadingSpinner({ className }: Props) {
   return (
-    <div className="relative flex h-full items-center justify-center">
+    <div
+      className={clsx(className, 'flex h-28 w-28 items-center justify-center')}
+    >
       <h2>Loading...</h2>
       <div className=" absolute h-28 w-28 animate-spin rounded-full border-b-2 border-t-2 border-red-500"></div>
     </div>
