a534afdb82a5563ae60176ce8b85b25a42c805b0
Oleksii Shevchenko(Mac) <rembo.51423@gmail.com>
2024-06-07 16:43:00 +0300

    feat(utils/getUserRecommendations): create util & add consts min_limit_rmdrs & add SUPPORT_EMAIL

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -4,4 +4,10 @@ export enum ApiRoutes {
   RESTAURANTS = 'restaurants',
   USERS = 'users',
   FAVORITES = 'favorites',
+  RECOMMENDATIONS = 'restaurants/recommendations',
 }
+
+export const MIN_FAVORITES_FOR_RECOMMENDATIONS = 2;
+export const DEFAULT_LIMIT_RECOMMENDATIONS = 4;
+
+export const SUPPORT_EMAIL = 'support@table.bar.com';
