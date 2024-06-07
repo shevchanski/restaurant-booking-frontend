630f10144be2c47fbc77708bd1505f8b72e3f6ab
Oleksii Shevchenko(Mac) <rembo.51423@gmail.com>
2024-06-07 16:34:05 +0300

    style(comps/CardsGrid): add grid-cols for diff breakpoints

--- src/components/CardsGrid/CardsGrid.tsx
+++ src/components/CardsGrid/CardsGrid.tsx
@@ -10,15 +10,19 @@ interface Props {
 export default function CardsGrid({ restaurants, favorites = [] }: Props) {
   return (
     <div
-      className={clsx(`grid w-fit grid-cols-3 gap-4 min-[1200px]:grid-cols-4`)}
+      className={clsx(
+        `grid w-fit grid-cols-2 gap-4 md:grid-cols-3 min-[1200px]:grid-cols-4`,
+      )}
     >
-      {...restaurants.map((rest, i) => (
-        <RestaurantCard
-          key={i}
-          restaurant={rest}
-          isFavorite={favorites.includes(rest._id)}
-        />
-      ))}
+      {...restaurants.map((rest, i) => {
+        return (
+          <RestaurantCard
+            key={i}
+            restaurant={rest}
+            isFavorite={favorites.includes(rest._id)}
+          />
+        );
+      })}
     </div>
   );
 }
