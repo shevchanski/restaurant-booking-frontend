061c17d6d35eeefdb10190dbfed0499ec319ebfb
Oleksii Shevchenko(Mac) <rembo.51423@gmail.com>
2024-06-08 20:06:14 +0300

    style(comps/Wrapper): add prop className

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -1,9 +1,15 @@
+import clsx from 'clsx';
 import React from 'react';
 
 interface Props {
   children: React.ReactNode;
+  className?: string;
 }
 
-export default function Wrapper({ children }: Props) {
-  return <div className="mx-auto w-full max-w-[1280px] px-5">{children}</div>;
+export default function Wrapper({ children, className }: Props) {
+  return (
+    <div className={clsx('mx-auto w-full max-w-[1280px] px-5', className)}>
+      {children}
+    </div>
+  );
 }
