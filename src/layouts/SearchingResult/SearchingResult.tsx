b2f8a0e28a48f4f4424b62167d6f9a3f4c1e8af6
Oleksii Shevchenko(Mac) <rembo.51423@gmail.com>
2024-06-09 23:53:32 +0300

    feat(layouts/SearchingResult): add InfoBox & Attention to show info to user

--- src/layouts/SearchingResult/SearchingResult.tsx
+++ src/layouts/SearchingResult/SearchingResult.tsx
@@ -1,8 +1,10 @@
+import Attention from '@/components/Attention/Attention';
+import InfoBox from '@/components/InfoBox/InfoBox';
+import { SUPPORT_EMAIL } from '@/constants/config';
 import ApiService from '@/services/api';
 import { SearchParams } from '@/types/search.type';
 import { auth } from '@clerk/nextjs/server';
-import { FaceSmileIcon } from '@heroicons/react/24/outline';
-import { ExclamationCircleIcon } from '@heroicons/react/24/solid';
+import clsx from 'clsx';
 import H2 from '../../components/H2/H2';
 import PaginationBar from '../../components/PaginationBar/PaginationBar';
 import SortSelector from '../../components/SortSelector/SortSelector';
@@ -37,13 +39,19 @@ export default async function SearchingResult({ searchParams }: Props) {
       : [];
 
   return (
-    <div className="w-fit px-5 md:px-0">
+    <>
       <H2>Searching Results</H2>
-      <div className="mt-5 grid grid-cols-[200px_1fr] gap-y-2">
-        <SortSelector className="col-start-2 justify-self-end" />
-        <div className="col-span-2 flex flex-col items-center">
-          {searchResponse && searchResponse.restaurants.length ? (
-            <>
+      <div
+        className={clsx('mt-5   ', {
+          'grid grid-cols-[200px_1fr] gap-y-2':
+            searchResponse?.restaurants.length,
+          'w-9/12': !searchResponse?.restaurants.length,
+        })}
+      >
+        {searchResponse && !!searchResponse.restaurants.length ? (
+          <>
+            <SortSelector className="col-start-2 justify-self-end" />
+            <div className="col-span-2 flex flex-col items-center">
               <CardsGrid
                 restaurants={searchResponse.restaurants}
                 favorites={userFavorites}
@@ -52,26 +60,45 @@ export default async function SearchingResult({ searchParams }: Props) {
                 className="mt-10"
                 totalAmount={searchResponse.totalAmount}
               />
-            </>
-          ) : (
-            <NoResultMessage />
-          )}
-        </div>
+            </div>
+          </>
+        ) : searchResponse && !searchResponse.restaurants.length ? (
+          <InfoBox className="flex flex-col items-center gap-1 text-base">
+            <h2 className=" text-center text-[24px] leading-10">
+              Nothing has matched your query.
+            </h2>
+            <p>
+              We apologize that nothing has been found according your entered
+              value.
+            </p>
+            <p>
+              Try to change the searching parameter or check that value is
+              correct.
+            </p>
+            <p>
+              We are constantly expanding our database of restaurants, so soon
+              here will be much more options for you to choose from.
+            </p>
+            <p>Thank you for understanding!</p>
+          </InfoBox>
+        ) : (
+          <Attention className="flex flex-col items-center text-center">
+            <h2 className="text-center text-[24px] leading-10">
+              Couldn`t connect with server!
+            </h2>
+            <p>
+              Try check your connection and reload that page few time.
+              <br /> If it doesn`t help, contact our support:{' '}
+              <a
+                href={`mailto:${SUPPORT_EMAIL}`}
+                className="font-medium hover:underline"
+              >
+                {SUPPORT_EMAIL}
+              </a>{' '}
+            </p>
+          </Attention>
+        )}
       </div>
-    </div>
-  );
-}
-
-function NoResultMessage() {
-  return (
-    <div className="flex flex-col items-center justify-center pt-[100px] ">
-      <ExclamationCircleIcon className="m-0 h-[60px] w-[60px] text-blue-500" />
-      <h2 className=" text-center text-[36px] text-gray-600 ">
-        Sorry :(
-        <br /> But nothing found on your request
-        <br />
-        Try one more time <FaceSmileIcon className=" inline-block h-12 w-12" />
-      </h2>
-    </div>
+    </>
   );
 }
