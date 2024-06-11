bf3a4bdfc3f46a608b87973759e1d23f1428b2d4
Oleksii Shevchenko(Mac) <rembo.51423@gmail.com>
2024-06-11 15:57:55 +0300

    feat(comps/Input): add validErrors prop & style for invalid state

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,11 +1,13 @@
 import uniqueHtmlId from '@/utils/uniqueHtmlId';
 import clsx from 'clsx';
 import React, { forwardRef } from 'react';
+import { FieldErrors } from 'react-hook-form';
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   labelText: React.ReactNode;
   inputIcon?: React.ReactNode;
   errorMessage?: string;
+  validErrors?: FieldErrors;
 }
 
 /**
@@ -14,10 +16,30 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
  * @returns {React.ReactNode}
  */
 const Input = forwardRef<HTMLInputElement, Props>(function Input(
-  { labelText, className, inputIcon, required, errorMessage, ...rest }: Props,
+  {
+    labelText,
+    className,
+    inputIcon,
+    required,
+    errorMessage,
+    name,
+    validErrors,
+    ...rest
+  }: Props,
   ref,
 ) {
   const _id = uniqueHtmlId('input-');
+  const isInvalid = errorMessage
+    ? true
+    : validErrors && name && validErrors[name]
+      ? true
+      : false;
+
+  const errorMsg = errorMessage
+    ? errorMessage
+    : validErrors && name
+      ? validErrors[name]?.message?.toString()
+      : undefined;
 
   return (
     <div className={className}>
@@ -35,9 +57,10 @@ const Input = forwardRef<HTMLInputElement, Props>(function Input(
       <div className="relative">
         <input
           className={clsx(
-            'aria-disabled: peer block w-full rounded-md border border-gray-200 px-[12px] py-[9px] text-sm outline-2 placeholder:text-gray-500',
+            'aria-disabled: invalid:[&not:(:placeholder-shown)]:border-red-500 focus:invalid:[&not:(:placeholder-shown)]:ring-red-500 peer block w-full rounded-md border border-gray-200 px-[12px] py-[9px] text-sm  outline-2  placeholder:text-gray-500',
             { 'pl-10': inputIcon },
           )}
+          name={name}
           id={_id}
           ref={ref}
           required={required}
@@ -50,7 +73,18 @@ const Input = forwardRef<HTMLInputElement, Props>(function Input(
           </div>
         )}
       </div>
-      {errorMessage && <p className="">{errorMessage}</p>}
+      <p
+        role="alert"
+        className={clsx(
+          'mb-1 mt-[2px] h-4 pl-2 text-xs text-red-600 duration-75',
+          {
+            'opacity-0': !isInvalid,
+            'opacity-100': isInvalid,
+          },
+        )}
+      >
+        {errorMsg}
+      </p>
     </div>
   );
 });
