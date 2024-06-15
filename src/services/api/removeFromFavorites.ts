9fc5b490afd23df64df6c01e7e9935c1fe520759
Oleksii Shevchenko(Mac) <rembo.51423@gmail.com>
2024-06-15 21:54:39 +0300

    refactor(services/api): change api req params '/url/:userId' & req.data={restId} addToFavorites & removeFromFavorites

--- src/services/api/removeFromFavorites.ts
+++ src/services/api/removeFromFavorites.ts
@@ -1,14 +1,17 @@
 import { ApiRoutes } from '@/constants/config';
 import { FavoriteApiPayload } from '@/types/global.type';
 import ErrorLogger from '@/utils/ErrorLogger';
+import path from 'path';
 import { api_instance } from './axios_instance';
 
 export async function removeFromFavorites(
   data: FavoriteApiPayload,
 ): Promise<boolean> {
   try {
-    await api_instance.delete(ApiRoutes.FAVORITES, {
-      data: data,
+    const { userId, restaurantId } = data;
+
+    await api_instance.delete(path.join(ApiRoutes.FAVORITES, userId), {
+      data: { restaurantId },
     });
 
     return true;
