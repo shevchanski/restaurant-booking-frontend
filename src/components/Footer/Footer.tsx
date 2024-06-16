c6d0f4702c38d5ccafe6b72e24f34c9334b67ec7
Oleksii Shevchenko(Mac) <rembo.51423@gmail.com>
2024-06-16 13:02:50 +0300

    style(comps/Footer): add max-[375px]: flex-col & justify-evenly

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,11 +4,11 @@ export default function Footer() {
   return (
     <footer className=" h-14 w-full bg-slate-600 text-slate-300">
       <Wrapper className="h-full">
-        <div className="flex h-full items-center justify-between px-4">
+        <div className="flex h-full items-center justify-between max-[375px]:flex-col max-[375px]:justify-evenly md:px-4">
           <p className="text-sm">&copy; 2024 All rights reserved.</p>
           <p className="text-xs">
             Developed by{' '}
-            <a href="https://yourdeveloperlink.com" className="underline">
+            <a href="https://github.com/shevchanski" className="underline">
               Oleksii Shevchenko
             </a>
           </p>
